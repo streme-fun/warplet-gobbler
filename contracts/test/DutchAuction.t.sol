53a3ea4345e02b47c0b39b1542699164be8cf13f
kobuta23 <gadousinsin@gmail.com>
2026-03-30 17:18:38 +0530

    some simple tests

---
+++ contracts/test/DutchAuction.t.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {DutchAuction} from "../src/DutchAuction.sol";
+import {IDutchAuction} from "../src/interfaces/IDutchAuction.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockUSDCx is ERC20 {
+    constructor() ERC20("Mock USDCx", "USDCx") {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
+contract MockWarplets is ERC721 {
+    constructor() ERC721("Mock Warplets", "WARPLET") {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
+
+contract DutchAuctionTest is Test {
+    MockWarplets internal warplets;
+    MockUSDCx internal paymentToken;
+    DutchAuction internal auction;
+
+    address internal seller = makeAddr("seller");
+    address internal nftReserve = makeAddr("nftReserve");
+    address internal recoveryWallet = makeAddr("recoveryWallet");
+
+    uint256 internal constant TOKEN_ID = 1;
+    uint256 internal constant POT = 1_000e18;
+
+    function setUp() public {
+        warplets = new MockWarplets();
+        paymentToken = new MockUSDCx();
+        auction = new DutchAuction(address(warplets), address(paymentToken), nftReserve, recoveryWallet);
+
+        warplets.mint(seller, TOKEN_ID);
+        paymentToken.mint(address(auction), POT);
+    }
+
+    function test_constructor_sets_approval_for_recovery_wallet() public view {
+        uint256 allowance = paymentToken.allowance(address(auction), recoveryWallet);
+        assertEq(allowance, 100 ether);
+    }
+
+    function test_currentPrice_returns_payment_token_balance() public view {
+        assertEq(auction.currentPrice(), POT);
+    }
+
+    function test_gobble_reverts_when_min_price_is_higher_than_pot() public {
+        vm.prank(seller);
+        warplets.approve(address(auction), TOKEN_ID);
+
+        vm.expectRevert(bytes("Price is too low, try again later"));
+        vm.prank(seller);
+        auction.gobble(TOKEN_ID, POT + 1);
+
+        assertEq(warplets.ownerOf(TOKEN_ID), seller);
+    }
+
+    function test_gobble_transfers_nft_and_pays_full_pot_and_emits_event() public {
+        vm.prank(seller);
+        warplets.approve(address(auction), TOKEN_ID);
+
+        vm.expectEmit(true, true, false, true, address(auction));
+        emit IDutchAuction.Gobbled(seller, TOKEN_ID, POT);
+
+        vm.prank(seller);
+        auction.gobble(TOKEN_ID, POT);
+
+        assertEq(warplets.ownerOf(TOKEN_ID), nftReserve);
+        assertEq(paymentToken.balanceOf(address(auction)), 0);
+        assertEq(paymentToken.balanceOf(seller), POT);
+    }
+}
